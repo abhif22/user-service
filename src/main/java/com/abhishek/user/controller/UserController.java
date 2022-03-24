2f883e5235e8fa849cfff21423aa90a402872704
Abhishek Srivastava <abhishek4.srivastava@airtel.com>
2022-03-25 01:51:34 +0530

    adding kafka producer working

--- src/main/java/com/abhishek/user/controller/UserController.java
+++ src/main/java/com/abhishek/user/controller/UserController.java
@@ -5,20 +5,26 @@ import com.abhishek.user.entity.dto.UserResponse;
 import com.abhishek.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
 
+    private static final String TOPIC = "NewTopic";
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    KafkaTemplate<String, User> kafkaTemplate;
 
     @PostMapping("/")
     public User saveUser(@RequestBody User user) {
         log.info("Saving User with ID: "+user.getUserId());
+        kafkaTemplate.send(TOPIC, user);
         return userService.saveUser(user);
     }
 
